bdcf82f0d5b700179a3ace3250e23cd5eec84fbe
Scott Dalgliesh <scott.t.dalgliesh@gmail.com>
2025-01-10 20:09:11 -0500

    switch to PWM (LEDC) for stepper control

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_time::{Duration, Ticker};
 use esp_hal::gpio::{Input, Level, Output, Pull};
+use esp_hal::ledc;
 use esp_hal::prelude::*;
 use {defmt_rtt as _, esp_backtrace as _};
 
@@ -24,12 +24,33 @@ async fn main(_spawner: Spawner) {
     esp_hal_embassy::init(timg0.timer0);
 
     // initialize button GPIO
-    let wheel_button = Input::new(peripherals.GPIO6, Pull::Up);
+    let mut wheel_button = Input::new(peripherals.GPIO6, Pull::Up);
     let mut trigger_button = Input::new(peripherals.GPIO7, Pull::Up);
 
     // initialize stepper motor control GPIO
     let mut _dir = Output::new(peripherals.GPIO20, Level::High);
-    let mut step = Output::new(peripherals.GPIO21, Level::Low);
+    let step = Output::new(peripherals.GPIO21, Level::Low);
+
+    // initialize LEDC (for PWM control)
+    info!("Pulse frequency: {}", motor_config::MOTOR_FREQ);
+    let mut ledc = ledc::Ledc::new(peripherals.LEDC);
+    ledc.set_global_slow_clock(ledc::LSGlobalClkSource::APBClk);
+    let mut lstimer0 = ledc.timer::<ledc::LowSpeed>(ledc::timer::Number::Timer0);
+    lstimer0
+        .configure(ledc::timer::config::Config {
+            duty: ledc::timer::config::Duty::Duty8Bit,
+            clock_source: ledc::timer::LSClockSource::APBClk,
+            frequency: motor_config::MOTOR_FREQ.Hz(),
+        })
+        .unwrap();
+    let mut pwm_channel = ledc.channel(ledc::channel::Number::Channel0, step);
+    pwm_channel
+        .configure(ledc::channel::config::Config {
+            timer: &lstimer0,
+            duty_pct: 0,
+            pin_config: ledc::channel::config::PinConfig::PushPull,
+        })
+        .unwrap();
 
     // event loop
     loop {
@@ -39,25 +60,14 @@ async fn main(_spawner: Spawner) {
         // if not in home position, move to home position
         if wheel_button.is_high() {
             info!("Moving motor to home position");
-            let mut ticker =
-                Ticker::every(Duration::from_micros(motor_config::DELAY_TIME_US.into()));
-            while wheel_button.is_high() {
-                step_motor(1, &mut ticker, &mut step).await;
-            }
+            pwm_channel.set_duty(50).unwrap();
+            wheel_button.wait_for_low().await;
+            pwm_channel.set_duty(0).unwrap();
         }
 
         info!("Triggered - starting wheel rotation");
-        let mut ticker = Ticker::every(Duration::from_micros(motor_config::DELAY_TIME_US.into()));
-        step_motor(motor_config::NUM_STEPS, &mut ticker, &mut step).await;
-    }
-}
-
-// advance motor one step
-async fn step_motor(steps: u32, ticker: &mut Ticker, step_pin: &mut Output<'_>) {
-    for _ in 0..steps as usize {
-        ticker.next().await;
-        step_pin.set_high();
-        ticker.next().await;
-        step_pin.set_low();
+        pwm_channel.set_duty(50).unwrap();
+        wheel_button.wait_for_falling_edge().await;
+        pwm_channel.set_duty(0).unwrap();
     }
 }
--- src/motor_config.rs
+++ src/motor_config.rs
@@ -4,9 +4,8 @@
 pub const MOTOR_STEPS_PER_REV: u32 = 200;
 pub const MICRO_STEP_MODE_DIVISOR: u32 = 2;
 pub const RPM: u32 = 320;
-pub const GEAR_RATIO: u32 = 16;
 
 // Calculated values
-pub const NUM_STEPS: u32 = GEAR_RATIO * MOTOR_STEPS_PER_REV * MICRO_STEP_MODE_DIVISOR;
 pub const DELAY_TIME_US: u32 =
     60 * 1_000_000 / (2 * RPM * MOTOR_STEPS_PER_REV * MICRO_STEP_MODE_DIVISOR);
+pub const MOTOR_FREQ: u32 = 1_000_000 / (2 * DELAY_TIME_US);
