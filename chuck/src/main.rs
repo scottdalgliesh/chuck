b6710d3f7ee45f130fe413a0b74d1cc5bf4df00d
Scott Dalgliesh <scott.t.dalgliesh@gmail.com>
2025-03-23 15:23:23 -0400

    add support for ramped stepper acceleration

--- chuck/src/main.rs
+++ chuck/src/main.rs
@@ -1,20 +1,14 @@
 #![no_std]
 #![no_main]
 
-mod motor_config;
-
+use chuck_core::{Motor, MotorConfig, StepMode};
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_time::Timer;
 use esp_hal::gpio::{Input, InputConfig, Level, Output, OutputConfig, Pull};
-use esp_hal::ledc::{
-    self, LowSpeed,
-    channel::{self, Channel, ChannelIFace},
-    timer::{self, TimerIFace},
-};
-use esp_hal::time::Rate;
 use esp_hal::timer::timg::TimerGroup;
 use {defmt_rtt as _, esp_backtrace as _};
+
+const GEAR_RATIO: u32 = 16;
 
 #[esp_hal_embassy::main]
 async fn main(_spawner: Spawner) {
@@ -27,71 +21,38 @@ async fn main(_spawner: Spawner) {
 
     // initialize button GPIO
     let input_config = InputConfig::default().with_pull(Pull::Up);
-    let mut wheel_button = Input::new(peripherals.GPIO6, input_config);
+    let wheel_button = Input::new(peripherals.GPIO6, input_config);
     let mut trigger_button = Input::new(peripherals.GPIO7, input_config);
 
     // initialize stepper motor control GPIO
     let output_config = OutputConfig::default();
-    let mut _dir = Output::new(peripherals.GPIO20, Level::High, output_config);
-    let step = Output::new(peripherals.GPIO21, Level::Low, output_config);
+    let mut dir = Output::new(peripherals.GPIO20, Level::High, output_config);
+    let mut step = Output::new(peripherals.GPIO21, Level::Low, output_config);
 
-    // initialize LEDC (for PWM control)
-    info!("Pulse frequency: {}", motor_config::MOTOR_FREQ);
-    let mut ledc = ledc::Ledc::new(peripherals.LEDC);
-    let mut lstimer0 = ledc.timer::<ledc::LowSpeed>(timer::Number::Timer0);
-    let mut pwm_channel = ledc.channel(channel::Number::Channel0, step);
-    configure_pwm(
-        &mut ledc,
-        &mut lstimer0,
-        &mut pwm_channel,
-        motor_config::MOTOR_FREQ,
-    );
+    // initialize stepper motor driver
+    let motor_config = MotorConfig::new(200, StepMode::Full, 180, 640, 0.01, 2);
+    let mut motor = Motor {
+        step_pin: &mut step,
+        dir_pin: &mut dir,
+        config: &motor_config,
+    };
 
     // event loop
     loop {
         info!("Waiting for trigger button press");
+        info!("Delay time (us): {}", motor.config.min_step_period());
         trigger_button.wait_for_falling_edge().await;
 
         if wheel_button.is_high() {
-            // if not in home position, move to home position
             info!("Moving motor to home position");
-            pwm_channel.set_duty(50).unwrap();
-            wheel_button.wait_for_low().await;
-            pwm_channel.set_duty(0).unwrap();
+            while wheel_button.is_high() {
+                motor.step_once(motor.config.max_step_period()).await;
+            }
         } else {
-            // if in home position, rotate wheel one turn to fire
             info!("Triggered - starting wheel rotation");
-            pwm_channel.set_duty(50).unwrap();
-
-            // ignore limit switch input until half of wheel rotation is complete
-            // to debounce limit switch signal which is very noisy
-            Timer::after_micros((motor_config::WHEEL_PERIOD_US / 2).into()).await;
-            wheel_button.wait_for_falling_edge().await;
-            pwm_channel.set_duty(0).unwrap();
+            motor
+                .move_to_position(motor.config.steps_per_rev() * GEAR_RATIO)
+                .await;
         }
     }
-}
-
-/// Configure LEDC channel for PWM output
-fn configure_pwm<'a>(
-    ledc: &mut ledc::Ledc<'_>,
-    lstimer: &'a mut timer::Timer<'a, LowSpeed>,
-    pwm_channel: &mut Channel<'a, LowSpeed>,
-    freq: u32,
-) {
-    ledc.set_global_slow_clock(ledc::LSGlobalClkSource::APBClk);
-    lstimer
-        .configure(timer::config::Config {
-            duty: timer::config::Duty::Duty8Bit,
-            clock_source: timer::LSClockSource::APBClk,
-            frequency: Rate::from_hz(freq),
-        })
-        .unwrap();
-    pwm_channel
-        .configure(channel::config::Config {
-            timer: lstimer,
-            duty_pct: 0,
-            pin_config: channel::config::PinConfig::PushPull,
-        })
-        .unwrap();
 }
--- chuck_core/src/lib.rs
+++ chuck_core/src/lib.rs
@@ -1,12 +1,158 @@
 #![no_std]
+
+use embassy_futures::join::join;
+use embassy_time::Timer;
+use embedded_hal::digital::OutputPin;
+
+/// Micro-step mode for DRV8825 stepper motor.
+#[derive(Debug, Clone, Copy)]
+pub enum StepMode {
+    Full = 1,
+    Half = 2,
+    Quarter = 4,
+    Eighth = 8,
+    Sixteenth = 16,
+    ThirtySecondth = 32,
+}
+
+impl StepMode {
+    pub fn value(self) -> u32 {
+        self as u32
+    }
+}
+
+/// Configuration parameters for DRV8825 stepper motor.
+#[derive(Debug)]
+pub struct MotorConfig {
+    full_steps_per_rev: u32,
+    step_mode: StepMode,
+    min_rpm: u32,
+    max_rpm: u32,
+    accel: f32,
+    min_pulse_on_us: u32,
+}
+
+impl MotorConfig {
+    pub fn new(
+        full_steps_per_rev: u32,
+        step_mode: StepMode,
+        min_rpm: u32,
+        max_rpm: u32,
+        accel: f32,
+        min_pulse_on_us: u32,
+    ) -> Self {
+        MotorConfig {
+            full_steps_per_rev,
+            step_mode,
+            min_rpm,
+            max_rpm,
+            accel,
+            min_pulse_on_us,
+        }
+    }
+
+    /// calculates total steps per motor revolution, based on motor full steps per rev and micro-step mode
+    pub fn steps_per_rev(&self) -> u32 {
+        self.full_steps_per_rev * self.step_mode.value()
+    }
+
+    /// min step period (micros)
+    pub fn min_step_period(&self) -> u32 {
+        step_period_micros(self.max_rpm, self.full_steps_per_rev, self.step_mode)
+    }
+
+    /// max step period (micros)
+    pub fn max_step_period(&self) -> u32 {
+        step_period_micros(self.min_rpm, self.full_steps_per_rev, self.step_mode)
+    }
+}
+
+/// calculates step period (micros) based on target motor rpm, full steps per rev, and micro-step mode
+fn step_period_micros(rpm: u32, full_steps_per_rev: u32, step_mode: StepMode) -> u32 {
+    let freq = step_frequency(rpm, full_steps_per_rev, step_mode);
+    1_000_000 / freq
+}
+
+/// calculates step frequency (hz) based on target motor rpm, full steps per rev, and micro-step mode
+fn step_frequency(rpm: u32, full_steps_per_rev: u32, step_mode: StepMode) -> u32 {
+    full_steps_per_rev * step_mode.value() * rpm / 60
+}
+
+/// basic stepper motor driver with support for linearly-ramped acceleration
+#[derive(Debug)]
+pub struct Motor<'a, Step: OutputPin, Dir: OutputPin> {
+    pub step_pin: &'a mut Step,
+    pub dir_pin: &'a mut Dir,
+    pub config: &'a MotorConfig,
+}
+
+impl<Step: OutputPin, Dir: OutputPin> Motor<'_, Step, Dir> {
+    /// move motor by one step
+    pub async fn step_once(&mut self, period_us: u32) {
+        join(Timer::after_micros(period_us.into()), async {
+            let _ = self.step_pin.set_high();
+            Timer::after_micros(self.config.min_pulse_on_us.into()).await;
+            let _ = self.step_pin.set_low();
+        })
+        .await;
+    }
+
+    /// move specified number of steps, starting from min_rpm and observing max accel  
+    pub async fn move_to_position(&mut self, steps: u32) {
+        let init_step_period = self.config.max_step_period();
+        let target_step_period = self.config.min_step_period();
+        let ramp_increment = (init_step_period as f32 * self.config.accel) as u32;
+        let max_ramp_steps = (init_step_period - target_step_period) / ramp_increment;
+        let ramp_steps = if 2 * max_ramp_steps >= steps {
+            steps / 2
+        } else {
+            max_ramp_steps
+        };
+        let constant_steps = steps - 2 * ramp_steps;
+
+        // ramp-up
+        let mut current_period = init_step_period;
+        for _ in 0..ramp_steps {
+            self.step_once(current_period).await;
+            current_period -= ramp_increment;
+        }
+
+        // full-speed
+        for _ in 0..constant_steps {
+            self.step_once(target_step_period).await;
+        }
+
+        // ramp-down
+        let mut current_period = target_step_period;
+        for _ in 0..ramp_steps {
+            self.step_once(current_period).await;
+            current_period += ramp_increment;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     extern crate std;
-    use std::println;
+    use super::*;
+    use std::prelude::rust_2024::*;
 
     #[test]
-    fn sample_test() {
-        println!("Sample test is running.");
-        assert!(true);
+    fn step_mode_test() {
+        assert_eq!(StepMode::Full.value(), 1_u32);
+        assert_eq!(StepMode::Quarter.value(), 4_u32);
+    }
+
+    #[test]
+    fn motor_config_test() {
+        let config = MotorConfig::new(200, StepMode::Half, 120, 360, 0.01, 2);
+        assert_eq!(config.steps_per_rev(), 400);
+        assert_eq!(config.min_step_period(), 416);
+        assert_eq!(config.max_step_period(), 1_250);
+
+        let config = MotorConfig::new(200, StepMode::Eighth, 147, 412, 0.01, 2);
+        assert_eq!(config.steps_per_rev(), 1_600);
+        assert_eq!(config.min_step_period(), 91);
+        assert_eq!(config.max_step_period(), 255);
     }
 }
